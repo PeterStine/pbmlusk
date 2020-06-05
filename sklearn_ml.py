536c9c23936d1c5335e6d521bd3b08bba582e1dd
Peter Robert Stine <peter.stine@oit.edu>
2020-06-05 15:38:57 -0700

    Loaded train and test into variables

--- sklearn_ml.py
+++ sklearn_ml.py
@@ -10,3 +10,9 @@ from sklearn import datasets
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+
+iris = datasets.load_iris()
+x = iris.data   #train
+y = iris.target #test
+
+
